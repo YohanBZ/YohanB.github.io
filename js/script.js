ac56510ef1da7337ad700cde5d0c7d89afa84f19
YohanBZ <barbozayohan@gmail.com>
2023-02-07 17:36:22 -0300

    addScript

--- js/script.js
+++ js/script.js
@@ -15,3 +15,28 @@ function toggleMenu(event) {
 
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu, { passive: true });
+
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll('.js-scroll');
+
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.7;
+
+    function animascroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowMetade < 0;
+        if (isSectionVisible) {
+          section.classList.add('ativo');
+        } else {
+          section.classList.remove('ativo');
+        }
+      });
+    }
+    animascroll();
+
+    window.addEventListener('scroll', animascroll);
+  }
+}
+
+initAnimacaoScroll();
